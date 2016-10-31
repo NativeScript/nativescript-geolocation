8f398693feef2296a3ee656cea8a25ed35230053
Nedyalko Nikolov <nedyalko.nikolov@telerik.com>
2016-10-31 15:05:26 +0200

    Fixed compile.sh script to work correctly on ios.

--- compile.sh
+++ compile.sh
@@ -8,7 +8,7 @@ copy_package_files() {
     cp "$SOURCE_DIR"/package.json $PACK_DIR
     cp "$SOURCE_DIR"/*.md $PACK_DIR
 	cp "$SOURCE_DIR"/*.d.ts $PACK_DIR
-    cp -r "$SOURCE_DIR"/platforms/ $PACK_DIR
+    cp -r "$SOURCE_DIR"/platforms $PACK_DIR
 }
 
 #clean dist folder from previous compilation
