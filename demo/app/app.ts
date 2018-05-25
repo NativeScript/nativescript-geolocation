562b255fc3150cae23b1c424c20256ed32e2a0cf
Stoyan Stratev <stoyan.stratev@telerik.com>
2018-05-25 14:31:19 +0300

    feat: update webpack in demo

--- demo/app/app.ts
+++ demo/app/app.ts
@@ -1,3 +1,2 @@
-﻿import "./bundle-config";
-import * as application from 'tns-core-modules/application';
+﻿import * as application from 'tns-core-modules/application';
 application.start({ moduleName: "main-page" });
